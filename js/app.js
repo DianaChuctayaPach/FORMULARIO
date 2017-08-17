febdad927fc0e412867b8852ec1e0ec86eb8049a
diana chuctaya pacheco <leydiana.ch2015@gmail.com>
2017-08-17 18:35:23 -0500

    maquetacion

---
+++ js/app.js
@@ -0,0 +1,16 @@
+ function validar() {
+var nombre = document.getElementById("nombre").value;
+var apellido = document.getElementById('apellido').value;
+var fecha = document.getElementById('fecha').value;
+var dni =document.getElementsByTagName('dni').value;
+var celular = document.getElementById('celular').value;
+var contraceña = document.getElementById('contraceña').value;
+var confirmacion_contraceña =document.getElementById('confir-contraceña').value;
+var comentarios = document.getElementById('comentarios').value;
+var suscripcion = document.getElementById('suscripcion').value;
+var registrar = document.getElementById('registrar').value;
+Console.log(nombre);
+    if (nombre =="" &) {
+         alert("El campo nombre esta vacia ");
+     }
+}
